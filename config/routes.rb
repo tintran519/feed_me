cf8661ade840c5f6a48ae4b3f86d795dcb020ead
Tin Tran <tin.tran0519@gmail.com>
2016-10-29 17:50:34 -0700

    add session controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
+  #resources :sessions, only: [:new, :create, :destroy]
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create', as: 'sessions'
+  get 'logout' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
